8436a2c6c089cce20a4fbc26ccc5faedd1e1bd6f
Kurt Kartaltepe <kkartaltepe@gmail.com>
2013-03-15 04:48:22 -0500

    added polling into the example

--- example.py
+++ example.py
@@ -17,6 +17,7 @@ def getTop4LWord(room, args):
     room.message("The top 4+ letter word so far is: " + top4LWord[0]
                  + "(" + str(top4LWord[1]) + ")")
 
+
 dictionary = dict() #volatile... of course...
 wordCount = dict()
 adminList = ["botsmcgee"]
@@ -34,18 +35,21 @@ class TestBot(ch.RoomManager):
         self.setFontSize(10)
         self.enableBg()
         self.enableRecording()
+        self.polling = dict()
+        self.voters = dict()
+        self.votes = dict()
+        commandList["startPoll"] = self.startPoll
+        commandList["endPoll"] = self.endPoll
+        commandList["lastPoll"] = self.lastPoll
     
     def onConnect(self, room):
         print("Connected")
+        self.polling[room.name] = False
     
     def onReconnect(self, room):
         print("Reconnected")
     
     def onDisconnect(self, room):
-        uniqueWords = 0
-        print("Most used word is \"" + topWord[0] + "\"(" + str(topWord[1]) + ")")
-        print("Most used 4+ letter word is \"" + top4LWord[0] + "\"(" + str(top4LWord[1]) + ")")
-        print("There were " + str(uniqueWords) + " different words captured")
         print("Disconnected")
     
     def onMessage(self, room, user, message):
@@ -53,6 +57,7 @@ class TestBot(ch.RoomManager):
             print(user.name, message.ip, message.body)
         else:
             print(formatMsg(message))
+        self.handlePolling(room, message)
         if(message.user.name in adminList and
            message.body[0]==TRIGGER_CHAR):
             handleCommand(room, message)
@@ -87,7 +92,36 @@ class TestBot(ch.RoomManager):
     
     def onPMLoginFail(self, PM):
         print("Login to PM system failed")
-
+    
+    def startPoll(self, room, args):
+        self.voters[room.name] = list()
+        self.votes[room.name] = dict()
+        self.polling[room.name] = True
+        room.message("Polling has begun please vote a number 0-9 (only your
+                     first number will be counted. You only get one vote)")
+    
+    def endPoll(self, room, args):
+        if not self.polling[room.name]:
+            return
+        self.polling[room.name] = False
+        topChoice, numVotes = getPollResults(self.votes[room.name])
+        room.message("The winner is: " + topChoice[0] + " (" +
+                     str(topChoice[1]) + "/" + str(numVotes) + ")")
+    
+    def lastPoll(self, room, args):
+        topChoice, numVotes = getPollResults(self.votes[room.name])
+        room.message("The last poll's winner was: " + topChoice[0] + " (" +
+                     str(topChoice[1]) + "/" + str(numVotes) + ")")
+    
+    def handlePolling(self, room, message):
+        if not self.polling[room.name]:
+            return
+        match = re.match("(?!Polling).*([\d])", message.body)
+        if match and not message.user.name in self.voters[room.name]:
+            match = match.group(1)
+            self.voters[room.name].append(message.user.name)
+            self.votes[room.name][match] = self.votes[room.name].get(match, 0) + 1
+    
 
 def formatMsg(message):
     return u"<" + time.ctime(message.time) + u">" + message.user.name + u": " + message.body
@@ -98,7 +132,7 @@ def analyzeMsg(message):
     normBody = re.sub("[^\w\s]","", body).lower()
     normBody = normBody.split(" ")
     for word in normBody:
-        wordCount[word] = wordCount.get(word, 0)+1
+        wordCount[word] = wordCount.get(word, 0) + 1
         if wordCount[word] > topWord[1]:
             topWord = (word, wordCount[word])
         if wordCount[word] > top4LWord[1] and len(word) > 3:
@@ -110,5 +144,13 @@ def handleCommand(room, message):
     args = None if len(commandAndArg)<2 else commandAndArg[1]
     commandList.get(command, lambda room, args: None)(room, args)
 
+def getPollResults(votes):
+    numVotes = 0
+    topChoice = "Nothing", 0
+    for choice in votes.keys():
+        numVotes += votes[choice]
+        if votes[choice] > topChoice[1]:
+            topChoice = choice, votes[choice]
+    return topChoice, numVotes
 
 if __name__ == "__main__": TestBot.easy_start()
